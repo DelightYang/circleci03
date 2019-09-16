169abf747818118fc2d6b2e4827d3a3067578f78
Yang <cutesunshine@icloud.com>
2019-09-16 16:19:49 +0800

    update

--- src/test/kotlin/com/learn/circleci03/controllers/GreetingControllerTest.kt
+++ src/test/kotlin/com/learn/circleci03/controllers/GreetingControllerTest.kt
@@ -4,9 +4,7 @@ package com.learn.circleci03.controllers
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 
-@WebMvcTest(GreetingController::class)
 class GreetingControllerTest {
 
     @Test
