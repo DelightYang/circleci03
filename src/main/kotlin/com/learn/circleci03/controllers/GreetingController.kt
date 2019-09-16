90df2a53830102083418ca9da24e54d7f14a561d
Yang <cutesunshine@icloud.com>
2019-09-16 15:36:31 +0800

    update

--- src/main/kotlin/com/learn/circleci03/controllers/GreetingController.kt
+++ src/main/kotlin/com/learn/circleci03/controllers/GreetingController.kt
@@ -1,6 +1,5 @@
 package com.learn.circleci03.controllers
 
-import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
--- src/test/kotlin/com/learn/circleci03/controllers/GreetingControllerTest.kt
+++ src/test/kotlin/com/learn/circleci03/controllers/GreetingControllerTest.kt
@@ -1,16 +1,18 @@
 package com.learn.circleci03.controllers
 
+
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.mockito.Mockito.mock
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 
-
+@WebMvcTest(GreetingController::class)
 class GreetingControllerTest {
 
     @Test
     fun should_return_greeting_messages() {
         val mockController = mock(GreetingController::class.java)
-        Assertions.assertThat(mockController.sayHello() == "\"Hello CircleCi\"")
+        Assertions.assertThat(mockController.sayHello() == "Hello CircleCi")
     }
 
 }
